fbc27de240ca4d168f4fee8363fec6edc0a15c56
vanderoo <vandertokang17@gmail.com>
2024-12-29 22:50:14 +0700

    implementasi team Page

--- resources/scripts/team.js
+++ resources/scripts/team.js
@@ -1,15 +1,23 @@
 const teamId = window.location.hash.substring(1);
 const userId = localStorage.getItem('userId');
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
 
   if (teamId) {
     renderTeamData(teamId);
+    renderTaskList();
+    renderAssigneeOption();
   }
 
   handleLeaveTeam();
   setupPopupHandlers();
   handleTaskFormSubmission();
+  initializeTaskListDragable();
+
+  document.getElementById('edit-updatable-task').addEventListener('click', handleEditTask);
+  document.getElementById('save-updatable-task').addEventListener('click', handleSaveTask);
+  document.getElementById('cancel-updatable-task').addEventListener('click', handleCancelEditTask);
+  document.getElementById('delete-updatable-task').addEventListener('click', handleDeleteTask);
 
   document.getElementById("copy-icon").addEventListener("click", function () {
     const classCode = document.getElementById("team_code").innerText;
@@ -28,6 +36,329 @@ document.addEventListener('DOMContentLoaded', () => {
       });
   });
 });
+
+function initializeTaskListDragable() {
+  document.querySelectorAll('.task-list').forEach(taskList => {
+    new Sortable(taskList, {
+      group: 'shared',
+      animation: 150,
+      ghostClass: 'sortable-ghost',
+      onEnd: (event) => {
+        const taskCard = event.item;
+        const taskId = taskCard.querySelector('.task-card-name').getAttribute('taskId');
+
+        const columnId = taskCard.closest('.column').id;
+        let progress = '';
+
+        switch (columnId) {
+          case 'todo-column':
+            progress = 'not_started';
+            break;
+          case 'doing-column':
+            progress = 'in_progress';
+            break;
+          case 'done-column':
+            progress = 'completed';
+            break;
+          default:
+            console.log('Unknown column');
+            return;
+        }
+
+        updateTaskStatus(taskId, progress);
+      },
+    });
+  });
+}
+
+function updateTaskStatus(taskId, progress) {
+  const updatedTask = {
+    progress: progress,
+    context: 'team',
+  };
+
+  fetch(`http://localhost:3000/task/${taskId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+    },
+    body: JSON.stringify(updatedTask),
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.code === 200) {
+        renderTaskList();
+      } else {
+        console.log(data);
+      }
+    });
+}
+
+async function fetchTasksByContext(context, contextId) {
+  try {
+    const apiUrl = `http://localhost:3000/tasks/by-context/${context}/${contextId}`;
+
+    const response = await fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Gagal mengambil event dari server.');
+    }
+
+    const tasks = await response.json();
+    console.log(tasks.data);
+    return tasks.data;
+  } catch (error) {
+    console.error('Error fetching tasks:', error);
+    Swal.fire({
+      title: 'Gagal Mengambil Data',
+      text: 'Terjadi kesalahan saat mengambil data tugas. Silakan coba lagi.',
+      icon: 'error',
+    });
+    return [];
+  }
+}
+
+async function renderAssigneeOption() {
+  try {
+    const accessToken = localStorage.getItem('accessToken');
+
+    if (!accessToken) {
+      throw new Error("Access token is missing");
+    }
+
+    const url = `http://localhost:3000/team/${teamId}/users`;
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch users data");
+    }
+
+    const data = await response.json();
+
+    const assigneeSelect = document.getElementById('task-assignee');
+    const assigneeDetailSelect = document.getElementById('show-updatable-task-assignee');
+
+    assigneeSelect.innerHTML = '<option value="" disabled selected>Pilih Assignee</option>';
+    assigneeDetailSelect.innerHTML = '<option value="" disabled selected>Pilih Assignee</option>';
+
+    data.data.forEach(user => {
+      const option = document.createElement('option');
+      option.value = user.id;
+      option.textContent = user.username;
+      assigneeSelect.appendChild(option);
+      assigneeDetailSelect.appendChild(option.cloneNode(true));
+    });
+
+  } catch (error) {
+    console.error("Error rendering assignee options:", error);
+    Swal.fire({
+      title: 'Gagal Mengambil Data Assignee',
+      text: 'Terjadi kesalahan saat mengambil data pengguna. Silakan coba lagi.',
+      icon: 'error',
+    });
+  }
+}
+
+async function renderTaskList() {
+  const todoColumn = document.getElementById('todo-column').querySelector('.task-list');
+  const doingColumn = document.getElementById('doing-column').querySelector('.task-list');
+  const doneColumn = document.getElementById('done-column').querySelector('.task-list');
+
+  todoColumn.innerHTML = '';
+  doingColumn.innerHTML = '';
+  doneColumn.innerHTML = '';
+
+  const tasks = await fetchTasksByContext('team', teamId);
+
+  tasks.forEach(task => {
+    const taskCard = document.createElement('div');
+    taskCard.classList.add('task-card');
+    const formattedDate = new Date(task.start_date)
+      .toLocaleString('sv-SE', { timeZone: 'Asia/Jakarta' })
+      .split(':')
+      .slice(0, -1)
+      .join(':');
+
+    taskCard.innerHTML = `
+    <p class="task-card-name" taskId="${task.id}">${task.name}</p>
+    <small class="task-card-deadline">Deadline: ${formattedDate}</small>
+    `;
+
+    taskCard.addEventListener('click', () => showTaskDetails(task));
+
+    switch (task.progress) {
+      case 'not_started':
+        todoColumn.appendChild(taskCard);
+        break;
+      case 'in_progress':
+        doingColumn.appendChild(taskCard);
+        break;
+      case 'completed':
+        doneColumn.appendChild(taskCard);
+        break;
+      default:
+        console.log('Unknown progress state');
+    }
+  });
+}
+
+const showUpdatableTaskPopup = document.getElementById("show-updatable-task-popup");
+const closeShowUpdatableTaskPopup = document.getElementById("close-show-updatable-task-popup");
+const showUpdatableTaskName = document.getElementById("show-updatable-task-name");
+const showUpdatableTaskDesc = document.getElementById("show-updatable-task-desc");
+const showUpdatableTaskDate = document.getElementById("show-updatable-task-date");
+const showUpdatableTaskAssignee = document.getElementById("show-updatable-task-assignee");
+const showUpdatableTaskProgress = document.getElementById("show-updatable-task-progress");
+
+function showTaskDetails(task) {
+  showUpdatableTaskName.value = task.name;
+  showUpdatableTaskName.setAttribute('taskId', task.id);
+  showUpdatableTaskDesc.value = task.description;
+  showUpdatableTaskDate.value = new Date(task.start_date).toISOString().slice(0, 16);
+  showUpdatableTaskAssignee.value = task.assignee;
+  showUpdatableTaskProgress.value = task.progress;
+
+  showUpdatableTaskPopup.style.display = 'flex';
+
+  closeShowUpdatableTaskPopup.addEventListener('click', hideUpdatableTaskDetails);
+}
+
+function hideUpdatableTaskDetails() {
+  showUpdatableTaskPopup.style.display = "none"; // Sembunyikan popup updatable task
+  handleCancelEditTask();
+}
+
+function handleEditTask() {
+  showUpdatableTaskName.disabled = false;
+  showUpdatableTaskDesc.disabled = false;
+  showUpdatableTaskDate.disabled = false;
+  showUpdatableTaskAssignee.disabled = false;
+  showUpdatableTaskProgress.disabled = false;
+
+  document.getElementById('edit-updatable-task').style.display = 'none';
+  document.getElementById('update-actions').style.display = 'block';
+}
+
+function handleCancelEditTask() {
+  showUpdatableTaskName.disabled = true;
+  showUpdatableTaskDesc.disabled = true;
+  showUpdatableTaskDate.disabled = true;
+  showUpdatableTaskAssignee.disabled = true;
+  showUpdatableTaskProgress.disabled = true;
+
+  document.getElementById('edit-updatable-task').style.display = 'inline-block';
+  document.getElementById('update-actions').style.display = 'none';
+}
+
+function handleSaveTask() {
+  const taskId = showUpdatableTaskName.getAttribute('taskId');
+  const updatedTask = {
+    name: showUpdatableTaskName.value,
+    description: showUpdatableTaskDesc.value,
+    start_date: showUpdatableTaskDate.value,
+    assignee: showUpdatableTaskAssignee.value,
+    progress: showUpdatableTaskProgress.value,
+    context: 'team',
+  };
+
+  fetch(`http://localhost:3000/task/${taskId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+    },
+    body: JSON.stringify(updatedTask),
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.code === 200) {
+        Swal.fire({
+          title: 'Tugas Diperbarui!',
+          text: 'Tugas berhasil diperbarui.',
+          icon: 'success',
+        }).then(() => {
+          hideUpdatableTaskDetails();
+          handleCancelEditTask();
+          renderTaskList();
+        });
+      } else {
+        console.log(data);
+        Swal.fire({
+          title: 'Gagal Memperbarui',
+          text: data.errors[0].message,
+          icon: 'error',
+        });
+      }
+    });
+}
+
+// Fungsi untuk menangani aksi penghapusan task
+function handleDeleteTask() {
+  const accessToken = localStorage.getItem('accessToken');
+  const taskId = showUpdatableTaskName.getAttribute('taskId');
+
+  Swal.fire({
+    title: 'Apakah Anda yakin?',
+    text: "Tugas ini akan dihapus secara permanen!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Ya, Hapus',
+    cancelButtonText: 'Batal',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      fetch(`http://localhost:3000/task/${taskId}`, {
+        method: 'DELETE',
+        headers: {
+          'Authorization': `Bearer ${accessToken}`,
+        },
+      })
+        .then(response => response.json())  // Mengambil body response dalam bentuk JSON
+        .then(data => {
+          console.log(data);
+          if (data.errors && data.errors.length > 0) {
+            Swal.fire({
+              title: 'Gagal Menghapus',
+              text: data.errors[0].message,
+              icon: 'error',
+            }).then(() => {
+              hideUpdatableTaskDetails();
+            });
+          } else {
+            Swal.fire({
+              title: 'Tugas Terhapus!',
+              text: 'Tugas berhasil dihapus.',
+              icon: 'success',
+            }).then(() => {
+              hideUpdatableTaskDetails();
+              renderTaskList();
+            });
+          }
+        })
+        .catch(error => {
+          // Menangani kesalahan jaringan atau server
+          Swal.fire({
+            title: 'Error',
+            text: error.message || 'Terjadi kesalahan jaringan.',
+            icon: 'error',
+          });
+        });
+    }
+  });
+}
 
 function renderTeamData(teamId) {
   const teamCodeElement = document.getElementById('team_code');
@@ -174,6 +505,7 @@ function handleTaskFormSubmission() {
           title: 'Berhasil!',
           text: 'Task berhasil ditambahkan!',
         });
+        renderTaskList();
         console.log('Task berhasil ditambahkan:', result.data);
       } else {
         handleApiError(result.error);
@@ -194,15 +526,23 @@ function handleTaskFormSubmission() {
 
 // Fungsi untuk mengambil data dari form tugas
 function getTaskFormData() {
+  const taskName = document.getElementById('task-name').value;
+  const taskDescription = document.getElementById('task-desc').value || "tbd";
   const taskDateValue = document.getElementById('task-date').value;
+  const assigneeValue = document.getElementById('task-assignee').value;
+  const progressValue = document.getElementById('task-progress').value;
+
   return {
-    name: document.getElementById('task-name').value,
-    description: document.getElementById('task-desc').value || "tbd",
+    name: taskName,
+    description: taskDescription,
     start_date: new Date(taskDateValue).toISOString(),
-    context: "personal",
-    user_id: localStorage.getItem('userId'),
+    assignee: assigneeValue,
+    progress: progressValue,
+    context: "team",
+    team_id: teamId,
   };
 }
+
 
 // Fungsi untuk mengirim data tugas ke API
 async function submitTaskData(taskData, accessToken) {
